f59045d4836f7f2e6fa36c1e70474dbabcdae6a0
iregon <0iregon0@gmail.com>
2020-04-24 16:19:49 +0200

    Add MQTT toggle component

---
+++ src/app/components/mqtt-toggle/mqtt-toggle.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mqtt-toggle',
+  templateUrl: './mqtt-toggle.component.html',
+  styleUrls: ['./mqtt-toggle.component.scss'],
+})
+export class MqttToggleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
